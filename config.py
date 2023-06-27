2f65e89fe15bf79c888b9efdca67dce7b13340bf
Laudry Iza Fadian <skakmate87@gmail.com>
2023-06-27 20:12:17 +0700

    update to url mongo atlas

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 class Config:
     # MongoDB configuration
     MONGODB_SETTINGS = {
-        'host': os.environ.get('MONGODB_URI', 'mongodb://localhost:3001/cobalagi')
+        'host': os.environ.get('MONGODB_URI', 'mongodb+srv://laudryfadian5:TwpKPpjzsLhXK0tD@cluster0.wvjlfip.mongodb.net/absensek?retryWrites=true&w=majority')
     }
